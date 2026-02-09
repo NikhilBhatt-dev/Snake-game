7f1cbceaa2f74570806dabcc697b8d907b811135
Nikhil Bhatt <bhattnikhil158@gmail.com>
2026-02-09 13:08:13 +0530

    working on js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-var hash  = document.getElementById("hash");
+const board = document.querySelector('.borad');
+const blockHeight = 30
+const blockWidth = 30
+
+const cols = Math.floor(board.clientWidth / blockWidth);
+const rows = Math.floor(board.clientHeight / blockHeight);
